892db06eb6f4eaa9081c56f9200dfa944ab59d81
Brendan Betheldo <bbetheldo@gmail.com>
2018-05-27 21:03:12 +1000

    Added more gulp tasks, fixed up html file. Restored site to previous state, while trimming all duplicated sources

--- app/js/plugins.js
+++ app/js/plugins.js
@@ -1,7 +1,7 @@
 // Avoid `console` errors in browsers that lack a console.
 (function() {
     var method;
-    var noop = function () {};
+    var noop = function() {};
     var methods = [
         'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
         'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
@@ -21,25 +21,31 @@
     }
 }());
 
- (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X');ga('send','pageview');
+(function(b, o, i, l, e, r) {
+    b.GoogleAnalyticsObject = l;
+    b[l] || (b[l] =
+        function() {
+            (b[l].q = b[l].q || []).push(arguments) });
+    b[l].l = +new Date;
+    e = o.createElement(i);
+    r = o.getElementsByTagName(i)[0];
+    e.src = '//www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e, r)
+}(window, document, 'script', 'ga'));
+ga('create', 'UA-XXXXX-X');
+ga('send', 'pageview');
 
 // Place any jQuery/helper plugins in here.
 
 
-        $(document).ready(function() {
-            $('#fullpage').fullpage({
-                css3: true,
-                scrollingSpeed: 650,
-                loopBottom: true,
-                anchors: ['Home', 'Work', 'About', 'Contact'],
-                menu:'#menu',
-                'autoScrolling': false,
-                sectionSelector:'.content'
-            });
-        });
-
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        css3: true,
+        scrollingSpeed: 650,
+        loopBottom: true,
+        anchors: ['Home', 'Work', 'About', 'Contact'],
+        menu: '#menu',
+        'autoScrolling': false,
+        sectionSelector: '.content'
+    });
+});
